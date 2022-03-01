e72ea596bb649cb1135e831c5fd4a681e41ce4ca
Eunseok OH <lspk.lpg@gmail.com>
2022-03-01 22:37:06 +0900

    conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {ColorProvider} from "./contexts/color";
 import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox/>
       </div>
     </ColorProvider>
--- src/contexts/color.js
+++ src/contexts/color.js
@@ -2,30 +2,31 @@ import {createContext, useState} from "react";
 
 const ColorContext = createContext({
   state: {color: 'black', subcolor: 'red'},
-  action: {
+  actions: {
     setColor: () => {
     },
-    setSubColor: () => {
+    setSubcolor: () => {
     },
   }
-});
+})
 
 const ColorProvider = ({children}) => {
   const [color, setColor] = useState('black');
-  const [subColor, setSubColor] = useState('red');
+  const [subcolor, setSubcolor] = useState('red');
 
   const value = {
-    state: {color, subColor},
-    actions: {setColor, setSubColor}
-  };
-
+    state: {color, subcolor},
+    actions: {setColor, setSubcolor},
+  }
   return (
-    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+    <ColorContext.Provider value={value}>
+      {children}
+    </ColorContext.Provider>
   );
 }
 
 const {Consumer: ColorConsumer} = ColorContext;
 
-export {ColorProvider, ColorConsumer}
+export {ColorProvider, ColorConsumer};
 
 export default ColorContext;
